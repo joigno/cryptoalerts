e5f932dd0e5effa1db62d568db18f88ea8b83cc1
joigno <30682875+joigno@users.noreply.github.com>
2022-03-25 16:13:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -122,6 +122,47 @@ def process_alert_cash(alert, prices, cg, portfolios):
     return triggered, prices, msg_extra
 
 
+def process_alert_crypto(alert, prices, cg, portfolios):
+    min_trade_usd = float(alert['min_trade_usd'])
+    triggered = False
+    portfolio_name = alert['portfolio']
+    portfolio = portfolios[portfolio_name]
+    # cash value of non-USD assets
+    cash_value_cryptos, prices = update_prices_portfolio(portfolio,prices,cg)
+    print('cash_value_cryptos = ', cash_value_cryptos)
+
+    msg_extra = ''
+    cryptos_num = len(portfolio['portfolio_assets'].keys()) - 1
+    target_crypto_percentage = (1 / cryptos_num) * 100.0
+    target_crypto_value = (cash_value_cryptos / cryptos_num)
+
+    for asset in portfolio['portfolio_assets'].keys():
+        if crypto == 'usd':
+            continue
+        # cash value of 1 crypto asset
+        curr_amount = float(portfolio['portfolio_assets'][asset]['amount'])
+        print("amount %s = "% asset, curr_amount)
+        curr_value = curr_amount * prices[asset]
+        curr_crypto_percentage = 100.0 * curr_value / cash_value_cryptos
+
+        # analyze logical conditions
+        if alert['condition'] == '>':
+            triggered = curr_crypto_percentage > target_crypto_percentage + alert['value']
+            portfolio['cash_percentage'] = 0
+            portfolio['portfolio_assets']['usd']['amount'] = 0
+            msg_extra = calculate_rebalancing(cash_value_cryptos, 0.0, prices, portfolio, min_trade_usd)
+
+        elif alert['condition'] == '<':
+            triggered = curr_crypto_percentage < target_crypto_percentage - alert['value']
+            portfolio['cash_percentage'] = 0
+            portfolio['portfolio_assets']['usd']['amount'] = 0
+            msg_extra = calculate_rebalancing(cash_value_cryptos, 0.0, prices, portfolio, min_trade_usd)
+
+        if triggered:
+            break
+    return triggered, prices, msg_extra
+
+
 def run(portfolios=None, alerts=None, prices=None):
     print_hi('PyCharm')
     cg = CoinGeckoAPI()
@@ -138,7 +179,7 @@ def run(portfolios=None, alerts=None, prices=None):
     msg = ''
     status_sent = {}
     for alert in alerts:
-
+        print('--------------- processsing alert ---------------')
         if alert['type'] == 'single_asset':
             triggered, prices = process_alert_single(alert, prices, cg, portfolios)
             if triggered:
@@ -155,17 +196,26 @@ def run(portfolios=None, alerts=None, prices=None):
                 print(msg)
                 send_email(alert['recipient'].split(','), subject, msg)
 
-        # Send Status Message (Daily)
-        if datetime.datetime.now().hour == 12:
-            print('INFO: sending status email')
-            subject = 'INFO crypto-alerts: system is up and running'
-            msg = subject + '\n' + '<pre>' + json.dumps(portfolios, indent=4, sort_keys=True)  \
-                  + '\n' + json.dumps(alerts, indent=4, sort_keys=True) + '<pre/>'
-            recipients = alert['recipient'].split(',')
-            for recp in recipients:
-                if not recp in status_sent:
-                    send_email(alert['recipient'].split(','), subject, msg)
-                status_sent[recp] = True
+        elif alert['type'] == 'crypto_percentage':
+            triggered, prices, msg_extra = process_alert_crypto(alert, prices, cg, portfolios)
+            if triggered:
+                # Send Email
+                msg = alert['message'] + '<br/>\n' + msg_extra
+                subject = 'ALERT crypto-alerts: ' + alert['message']
+                print(msg)
+                send_email(alert['recipient'].split(','), subject, msg)
+
+    # Send Status Message (Daily)
+    if datetime.datetime.now().hour == 12:
+        print('INFO: sending status email')
+        subject = 'INFO crypto-alerts: system is up and running'
+        msg = subject + '\n' + '<pre>' + json.dumps(portfolios, indent=4, sort_keys=True)  \
+              + '\n' + json.dumps(alerts, indent=4, sort_keys=True) + '<pre/>'
+        recipients = alert['recipient'].split(',')
+        for recp in recipients:
+            if not recp in status_sent:
+                send_email(alert['recipient'].split(','), subject, msg)
+            status_sent[recp] = True
 
 
     return msg
